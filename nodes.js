9502e794613efab1ac26aa2170aec936799639b7
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-09-17 15:49:07 +0530

    second commmit

--- nodes.js
+++ nodes.js
@@ -1,5 +1,4 @@
-let text = document.getElementById("addtxt")
-let title = document.getElementById("addtitle")
+
 let addbtn = document.getElementById("addbtn")
 // it gives here so all notes not be remove when we refresh page
 shownotes();
@@ -9,24 +8,36 @@ addbtn.addEventListener("click", func1)
 
 function func1(x) {
 
-    let notes = localStorage.getItem("notes")
+    let text = document.getElementById("addtxt")
+    let title = document.getElementById("addtitle")
 
-    if (notes == null) {
-        notesobj = [];
+
+
+    let getnotes = localStorage.getItem("notes")
+
+    if (getnotes == null) {
+        setobj = [];
     }
     else {
-        // we parse value means string ko  array bna dia fir se because stringify  hmne array ko string me convert krke save kiya tha
-        notesobj = JSON.parse(notes);
+        setobj = JSON.parse(getnotes)
     }
-    notesobj.push(text.value)
+
+
+    // we add  title and value in one variable 
+    let title_text = { "title": title.value, "value": text.value };
+    // we push this value in this setobj
+    setobj.push(title_text)
+
+    localStorage.setItem("notes", JSON.stringify(setobj))
 
 
 
-    localStorage.setItem("notes", JSON.stringify(notesobj))
     text.value = "";
     title.value = "";
-    // we make a function for show notes on display and we call this funciton here
+
+    //  we call this funciton here because we show the value after refresh the page 
     shownotes();
+
 
 }
 
@@ -36,67 +47,72 @@ function func1(x) {
 
 
 function shownotes() {
+    let getnotes = localStorage.getItem("notes")
 
-
-
-    let notes = localStorage.getItem("notes")
-
-    if (notes == null) {
-        notesobj = [];
+    if (getnotes == null) {
+        setobj = []
     }
     else {
-
-        notesobj = JSON.parse(notes);
+        setobj = JSON.parse(getnotes)
     }
+
 
 
     let html = "";
-    notesobj.forEach(function (element, index) {
+    setobj.forEach(function (element, index) {
+
+
+
 
         html +=
             `<div class="card my-2 mx-2" style="width: 18rem;">
-   <div class="card-body card-data" >
-       <h5 class="card-title">note ${index}</h5>
-       <p class="card-text">${element}</p>
-       <button class="btn btn-primary" id="${index}" onclick="deleteNode(this.id)" > Delete </button>
-   </div>
-   </div>`
+    <div class="card-body card-data" >
+    <h5 class="card-title"> ${index + 1}. ${element.title}</h5>
+    <p class="card-text">${element.value}</p>
+    <button class="btn btn-primary" id="${index}" onclick="deleteNode(this.id)" > Delete </button>
+    </div>
+    </div>`
+
     });
 
 
-    let noteselm = document.getElementById("notes")
 
-if (notesobj.length != 0) {
-    
-    noteselm.innerHTML = html
-}
+    let shownotes = document.getElementById("notes")
 
-else{
-    noteselm.innerHTML = "please  write notes in notes box and add notes for show here all notes."
-}
+
+    if (setobj.length != 0) {
+
+        shownotes.innerHTML = html
+    }
+
+    else {
+        shownotes.innerHTML = "please  write notes in notes box and add notes for show here all notes."
+    }
+
+
 
 }
 
 
 
 // it is function it call when we click on delete buton because we give it onclick.this.id
-function deleteNode(index){
-   
 
+function deleteNode(index) {
 
-    let notes = localStorage.getItem("notes")
+    let getnotes = localStorage.getItem("notes")
 
-    if (notes == null) {
-        notesobj = [];
+    if (getnotes == null) {
+        setobj = []
     }
     else {
-
-        notesobj = JSON.parse(notes);
+        setobj = JSON.parse(getnotes)
     }
 
-    notesobj.splice(index,1);
 
-    localStorage.setItem("notes",JSON.stringify(notesobj))
+    setobj.splice(index, 1);
+
+
+    localStorage.setItem("notes", JSON.stringify(setobj))
     shownotes();
 
 }
@@ -106,35 +122,37 @@ function deleteNode(index){
 let searchtxt = document.getElementById("searchtxt")
 
 
-searchtxt.addEventListener("input",func3)
+searchtxt.addEventListener("input", func3)
 
 
-function func3(){
+function func3() {
     let input = searchtxt.value;
- 
- let data = document.getElementsByClassName("card-data")
-  
- let arrdata = Array.from(data);
- 
- arrdata.forEach(element => {
-     let cardata = element.getElementsByTagName("p")[0].innerText;
-   
 
-     if (cardata.includes(input)) {
-        element.style.display = "block";
-        
-    }
-    else{
-         element.style.display = "none";
+    let data = document.getElementsByClassName("card-data")
 
-     }
- })
+    let arrdata = Array.from(data);
+
+    arrdata.forEach(element => {
+        let cardata = element.getElementsByTagName("p")[0].innerText;
+
+
+        if (cardata.includes(input)) {
+            element.style.display = "block";
+
+        }
+        else {
+            element.style.display = "none";
+
+        }
+    })
 
 
 }
 
 let search = document.getElementById('searchbtn')
-search.addEventListener("click",function(e){
+search.addEventListener("click", function (e) {
     searchtxt.value = "";
 
 })
+
+
